35374de6f3dbdcacc8a395327bd0aa93363d5039
Jose7RT <jose7rt@gmail.com>
2025-12-19 16:40:42 +0100

    Se añade soporte para el RTOS ThreadX v6.4.0 integrado manualmente sobre la placa Core.
    El código fuente de ThreadX se incluye en "middleware/threadx", donde se define un CMakeLists.txt propio para construir el RTOS como una librería OBJECT, permitiendo su reutilización y un control explícito del proceso de build.
    
    Se introduce el directorio "platform" como capa de adaptación entre el hardware concreto de la placa y los módulos de software independientes. En particular, la integración específica de ThreadX para esta placa se ubica en "platform/threadx".
    
    La integración requiere:
    
    - Implementar tx_initialize_low_level.S, responsable de:
    - Inicializar el stack del sistema.
    - Configurar SysTick como fuente de temporización del RTOS.
    - Definir las interrupciones críticas usadas por ThreadX (SVC, PendSV y SysTick).
    - Definir tx_user.h para ajustar parámetros de ThreadX dependientes de la plataforma (tamaños de stack, uso de memoria estática, etc.).
    
    Para permitir el control completo del RTOS sobre las interrupciones del sistema, se desactiva la generación automática de handlers por STM32CubeMX para:
    
    - System service call via SWI instruction (SVC)
    - Pendable request for system service (PendSV)
    - System tick timer (SysTick)
    
    Esto es necesario porque ThreadX proporciona sus propios handlers para estas interrupciones, y cualquier implementación generada por la HAL interferiría con el funcionamiento del scheduler.
    
    Dado que SysTick pasa a ser de uso exclusivo del RTOS, la base de tiempo de la HAL se reconfigura para usar TIM6 como Timebase Source, evitando conflictos entre la HAL y ThreadX
    
    La integración utiliza memoria estática, evitando el uso de asignación dinámica, y define explícitamente los tamaños de stack requeridos para el sistema y los hilos, de acuerdo con las recomendaciones de ThreadX y los requisitos del STM32H7.
    
    Esta primera integración ha sido validada mediante una prueba de concepto funcional (parpadeo de LED bajo control del RTOS).

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -189,6 +189,28 @@ void MPU_Config(void)
 }
 
 /**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6)
+  {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
@@ -197,8 +219,7 @@ void Error_Handler(void)
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
@@ -213,8 +234,9 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
